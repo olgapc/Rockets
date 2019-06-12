cb56ab82c5ee30cb975cce0aacacc7dde0096d99
Olga <olgapc@gmail.com>
2019-06-12 15:01:46 +0200

    fase4 rockets

--- src/rockets/Rocket.java
+++ src/rockets/Rocket.java
@@ -5,6 +5,9 @@
  */
 package rockets;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author olga
@@ -12,13 +15,96 @@ package rockets;
 public class Rocket
 {
 
-    String idRocket;
-    int numThruster;
+    private String idRocket;
+    private int numThruster;
+    private List<Thruster> thrusters;
+    private double speed;
 
-    public Rocket(String idRocket, int numThruster)
+    public Rocket(String idRocket, int numThruster, int[] args)
     {
         this.idRocket = idRocket;
         this.numThruster = numThruster;
+        this.speed = 0;
+
+        this.thrusters = new ArrayList<>();
+
+        if (args.length != numThruster)
+        {
+            new Exception("numero arguments erroni");
+        }
+        for (int i = 0; i < numThruster; i++)
+        {
+            thrusters.add(new Thruster(args[i]));
+
+        }
+    }
+
+    public List<Thruster> getThrusters()
+    {
+        return thrusters;
+    }
+
+    // return integer list of Thrusters power
+    public List<Integer> getThrustersPower()
+    {
+        List<Integer> powers = new ArrayList<>();
+
+        for (Thruster thruster : this.thrusters)
+        {
+            powers.add(thruster.getPower());
+
+        }
+        return powers;
+    }
+
+    //return integer list of Thrusters actual power
+    public List<Integer> getThrustersActPower()
+    {
+        List<Integer> actPowers = new ArrayList<>();
+
+        for (Thruster thruster : this.thrusters)
+        {
+            actPowers.add(thruster.getActPower());
+
+        }
+        return actPowers;
+    }
+
+    //accelerate Rocket, the power of each thruster of a rocket accelerates 10
+//    public void speedUpRocket()
+//    {
+//
+//        this.speedUp();
+//
+////        System.out.println("Rocket " + this.getIdRocket() + " propulsors " + this.getThrustersActPower());
+////        this.setSpeed();
+//    }
+    public void speedUpRocket()
+    {
+
+        for (Thruster thruster : this.thrusters)
+        {
+
+            thruster.speedUpThrusters(this.getIdRocket());
+
+        }
+        System.out.println("Rocket " + this.getIdRocket() + " propulsors " + this.getThrustersActPower());
+        this.setSpeed();
+
+    }
+
+    //slow down Rocket, the power of each thruster of a rocket decrease 10
+    public void slowDownRocket()
+    {
+        for (Thruster thruster : this.thrusters)
+        {
+
+            thruster.slowDownThrusters();
+
+        }
+
+        System.out.println("Rocket " + this.getIdRocket() + " propulsors " + this.getThrustersActPower());
+        this.setSpeed();
     }
 
     public String getIdRocket()
@@ -29,6 +115,22 @@ public class Rocket
     public int getNumThruster()
     {
         return numThruster;
+    }
+
+    public double getSpeed()
+    {
+        return speed;
+    }
+
+    public void setSpeed()
+    {
+        int sumPower = 0;
+        for (Thruster thruster : thrusters)
+        {
+            sumPower = sumPower + thruster.getActPower();
+        }
+        this.speed = this.speed + 100.0 * Math.sqrt(sumPower);
+
     }
 
 }
--- src/rockets/RocketsExercise.java
+++ src/rockets/RocketsExercise.java
@@ -17,13 +17,108 @@ public class RocketsExercise
      */
     public static void main(String[] args)
     {
-        Rocket rocket1 = new Rocket("32WESSDS", 3);
-        Rocket rocket2 = new Rocket("LDSFJA32", 6);
 
-        System.out.println("Coet 1: " + rocket1.getIdRocket()
+        //powerList for rocket1
+        int[] powerList1 =
+        {
+            10, 30, 80
+        };
+        //create rocket1
+        Rocket rocket1 = new Rocket("32WESSDS", 3, powerList1);
+
+        //powerList for rocket2
+        int[] powerList2 =
+        {
+            30, 40, 50, 50, 30, 10
+        };
+        //create rocket2
+        Rocket rocket2 = new Rocket("LDSFJA32", 6, powerList2);
+
+        System.out.print("Coet 1: " + rocket1.getIdRocket()
                 + " té " + rocket1.getNumThruster() + " propulsors");
-        System.out.println("Coet 2: " + rocket2.getIdRocket()
+
+        printThrusterPower(rocket1);
+
+        System.out.print("\nCoet 2: " + rocket2.getIdRocket()
                 + " té " + rocket2.getNumThruster() + " propulsors");
+
+        printThrusterPower(rocket2);
+
+        printSpeed(rocket1);
+        printSpeed(rocket2);
+
+        rocket1.speedUpRocket();
+        rocket2.speedUpRocket();
+        rocket1.speedUpRocket();
+        rocket2.speedUpRocket();
+        rocket1.speedUpRocket();
+        rocket2.speedUpRocket();
+
+        printSpeed(rocket1);
+        printSpeed(rocket2);
+
+        rocket1.slowDownRocket();
+        rocket1.slowDownRocket();
+        rocket1.slowDownRocket();
+        rocket1.slowDownRocket();
+        rocket1.slowDownRocket();
+
+        rocket2.speedUpRocket();
+        rocket2.speedUpRocket();
+        rocket2.speedUpRocket();
+        rocket2.speedUpRocket();
+        rocket2.speedUpRocket();
+        rocket2.speedUpRocket();
+        rocket2.speedUpRocket();
+
+        printSpeed(rocket1);
+        printSpeed(rocket2);
+
+        rocket1.speedUpRocket();
+        rocket2.speedUpRocket();
+        rocket1.speedUpRocket();
+        rocket2.speedUpRocket();
+        rocket1.speedUpRocket();
+        rocket2.speedUpRocket();
+        rocket1.speedUpRocket();
+        rocket2.speedUpRocket();
+        rocket1.speedUpRocket();
+        rocket2.speedUpRocket();
+        rocket1.speedUpRocket();
+        rocket2.speedUpRocket();
+        rocket1.speedUpRocket();
+        rocket2.speedUpRocket();
+        rocket1.speedUpRocket();
+        rocket2.speedUpRocket();
+        rocket1.speedUpRocket();
+        rocket2.speedUpRocket();
+        rocket1.speedUpRocket();
+        rocket2.speedUpRocket();
+        rocket1.speedUpRocket();
+        rocket2.speedUpRocket();
+        rocket1.speedUpRocket();
+        rocket2.speedUpRocket();
+        rocket1.speedUpRocket();
+        rocket2.speedUpRocket();
+        rocket1.speedUpRocket();
+        rocket2.speedUpRocket();
+        rocket1.speedUpRocket();
+        rocket2.speedUpRocket();
+
+        printSpeed(rocket1);
+        printSpeed(rocket2);
     }
 
+    public static void printThrusterPower(Rocket rocket)
+    {
+        for (Thruster thruster : rocket.getThrusters())
+        {
+            System.out.print(", " + thruster.getPower());
+        }
+    }
+
+    public static void printSpeed(Rocket rocket)
+    {
+        System.out.println("\nLa velocitat actual de " + rocket.getIdRocket() + " es " + rocket.getSpeed());
+    }
 }
